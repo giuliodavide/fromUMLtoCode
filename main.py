2b20ee0ce09590bf5d4d1202d3f5c5d699166224
Giulio Davide Carparelli <giulio.davide.97@gmail.com>
2020-12-29 15:44:43 +0100

    some optimization

--- main.py
+++ main.py
@@ -1,47 +1,59 @@
 from xml.dom import minidom
 
+
+def write_code(classes, type):
+    for k in range(len(classes.childNodes[j].childNodes)):
+        if classes.childNodes[j].childNodes[k].nodeName == type:
+            if type == "Attribute":
+                f.write(classes.childNodes[j].childNodes[k].attributes["Name"].value + ", ")
+            if type == "Operation":
+                f.write("\n\tdef " + classes.childNodes[j].childNodes[k].attributes["Name"].value + "():\n")
+
+
+def clean_classes(temp):
+    temp_classes = []
+    for i in range(len(temp)):
+        if temp[i].parentNode.nodeName == "ModelChildren":
+            temp_classes.append(temp[i])
+    return temp_classes
+
+
 f = open("interface.py", "a")
-class_diagram = minidom.parse('case_use/class_diagram_3.xml')
-temp = class_diagram.getElementsByTagName('Class')
-classes = []
-attributes = []
-operations = []
-returning = []
-
-for i in range(len(temp)):
-    if temp[i].parentNode.nodeName == "ModelChildren":
-        classes.append(temp[i])
-
-temp.clear()
+class_diagram = minidom.parse('case_use/class_diagram.xml')
+classes = class_diagram.getElementsByTagName('Class')
+classes = clean_classes(classes)
 
 for i in range(len(classes)):
-    attributes.clear()
-    operations.clear()
     f.write("\n\nclass " + classes[i].attributes['Name'].value + ":\n\tdef __init__(self, ")
     for j in range(len(classes[i].childNodes)):
-        for k in range(len(classes[i].childNodes[j].childNodes)):
-            if classes[i].childNodes[j].childNodes[k].nodeName == "Attribute":
-                attributes.append(classes[i].childNodes[j].childNodes[k].attributes["Name"].value)
-            if classes[i].childNodes[j].childNodes[k].nodeName == "Operation":
-                operations.append(classes[i].childNodes[j].childNodes[k].attributes["Name"].value)
-                if len(classes[i].childNodes[j].childNodes[k].childNodes) == 0:
-                    returning.append(False)
-                for w in range(len(classes[i].childNodes[j].childNodes[k].childNodes)):
-                    if classes[i].childNodes[j].childNodes[k].childNodes[w].nodeName != "ReturnType":
-                        returning.append(False)
-                    else:
-                        returning.append(True)
-
-    for j in range(len(attributes)):
-        f.write(attributes[j] + ", ")
+        write_code(classes[i], "Attribute")
+        # for k in range(len(classes[i].childNodes[j].childNodes)):
+        #     if classes[i].childNodes[j].childNodes[k].nodeName == "Attribute":
+        #         # attributes.append(classes[i].childNodes[j].childNodes[k].attributes["Name"].value)
+        #         f.write(classes[i].childNodes[j].childNodes[k].attributes["Name"].value + ", ")
     f.write("):\n\t\t")
+    for j in range(len(classes[i].childNodes)):
+        write_code(classes[i], "Operation")
+    # if classes[i].childNodes[j].childNodes[k].nodeName == "Operation":
+    #     operations.append(classes[i].childNodes[j].childNodes[k].attributes["Name"].value)
+    #     if len(classes[i].childNodes[j].childNodes[k].childNodes) == 0:
+    #         returning.append(False)
+    #     for w in range(len(classes[i].childNodes[j].childNodes[k].childNodes)):
+    #         if classes[i].childNodes[j].childNodes[k].childNodes[w].nodeName != "ReturnType":
+    #             returning.append((False, k))
+    #         else:
+    #             returning.append((True, k))
 
-    for j in range(len(attributes)):
-        f.write("self." + attributes[j] + " = " + attributes[j] + "\n\t\t")
-
-    for j in range(len(operations)):
-        f.write("\n\tdef " + operations[j] + "():\n")
-        if returning[j]:
-            f.write("\t\treturn NotImplemented\n")
+    # for j in range(len(attributes)):
+    #     f.write(attributes[j] + ", ")
+    # f.write("):\n\t\t")
+    #
+    # for j in range(len(attributes)):
+    #     f.write("self." + attributes[j] + " = " + attributes[j] + "\n\t\t")
+    #
+    # for j in range(len(operations)):
+    #     f.write("\n\tdef " + operations[j] + "():\n")
+    #     if returning[k]:
+    #         f.write("\t\treturn NotImplemented\n")
 
 f.close()
