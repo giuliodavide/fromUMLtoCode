92e0cc01d2a694686e8806dccf070b119cf703c6
Giulio Davide Carparelli <giulio.davide.97@gmail.com>
2021-01-11 12:17:13 +0100

    finished the ordering

--- main.py
+++ main.py
@@ -3,57 +3,6 @@ import argparse
 import os
 from utility import *
 
-# def write_code(list, tag):
-#     for j in range(len(list.childNodes)):
-#         for k in range(len(list.childNodes[j].childNodes)):
-#             if list.childNodes[j].childNodes[k].nodeName == tag:
-#                 if tag == "Attribute":
-#                     f.write(list.childNodes[j].childNodes[k].attributes["Name"].value + ", ")
-#                     attributes.append(list.childNodes[j].childNodes[k].attributes["Name"].value)
-#                 if tag == "Operation":
-#                     f.write("\n\tdef " + list.childNodes[j].childNodes[k].attributes["Name"].value + "(self, ")
-#                     for w in range(len(list.childNodes[j].childNodes[k].childNodes)):
-#                         for z in range(len(list.childNodes[j].childNodes[k].childNodes[w].childNodes)):
-#                             if list.childNodes[j].childNodes[k].childNodes[w].childNodes[z].nodeName == "Parameter":
-#                                 f.write(list.childNodes[j].childNodes[k].childNodes[w].childNodes[z].attributes["Name"].value + ", ")
-#                     f.write("):\n\t\t# insert body here\n")
-#                     try:
-#                         list.childNodes[j].childNodes[k].attributes["ReturnType"]
-#                         f.write("\t\treturn NotImplemented\n")
-#                     except KeyError:
-#                         # here we don't want the program to do something
-#                         NotImplemented
-#                     if len(list.childNodes[j].childNodes[k].childNodes) == 0:
-#                         break
-#                     for w in range(len(list.childNodes[j].childNodes[k].childNodes)):
-#                         if list.childNodes[j].childNodes[k].childNodes[w].nodeName == "ReturnType":
-#                             f.write("\t\treturn NotImplemented\n")
-#
-#
-# def extract(temp_var, tag):
-#     temp = []
-#     for index in range(len(temp_var)):
-#         if temp_var[index].parentNode.nodeName == 'ModelChildren':
-#             if tag == 'Class':
-#                 temp.append(temp_var[index])
-#             if tag == 'Generalization':
-#                 temp.append((temp_var[index].attributes['From'].value, temp_var[index].attributes['To'].value))
-#     return temp
-#
-#
-# def verify(list):
-#     for i in range(len(fromTo)):
-#         if list.attributes["Id"].value == fromTo[i][1]:
-#             return association(fromTo[i][0])
-#     return ""
-#
-#
-# def association(id):
-#     for i in range(len(classes)):
-#         if id == classes[i].attributes["Id"].value:
-#             return classes[i].attributes["Name"].value
-#     return "NotDefined"
-
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-c", "--class", required=True,
